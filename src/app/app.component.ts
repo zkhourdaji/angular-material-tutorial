563c4857af37c14181e16c8802949e876f902047
Zafer Khourdaji <khourdaji@gmail.com>
2019-09-01 16:26:27 -0400

    Add custom material module. Demo some material components and directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
+import { MatButtonToggleGroup } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'angular-material-tutorial';
+
+  @ViewChild('myToggleGroup', { static: false })
+  myToggleGroup: ElementRef;
+
+  @ViewChild('toggleGroup', { static: false })
+  toggleGroup: MatButtonToggleGroup;
+
+  ngOnInit() {
+    console.log(this.myToggleGroup);
+  }
+
+  ngAfterViewInit() {
+    console.log(this.toggleGroup);
+    this.toggleGroup.change.subscribe((value) => value ? console.log(value) : false);
+  }
 }
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatButtonToggleModule } from '@angular/material';
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatButtonToggleModule
+];
+
+@NgModule({
+  imports: MaterialComponents,
+  exports: MaterialComponents
+})
+export class MaterialModule { }
